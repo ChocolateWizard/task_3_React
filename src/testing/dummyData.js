92fed1c2a972598146282bde2891a509b2d6eae6
ChocolateWizard <despotjev@gmail.com>
2023-05-03 16:11:11 +0200

    Initial skeleton of frontpage

---
+++ src/testing/dummyData.js
@@ -0,0 +1,36 @@
+export const movies = [
+  {
+    title: "The Lighthouse",
+    release_date: "2019-04-18",
+    cover_url:
+      "https://m.media-amazon.com/images/M/MV5BZmE0MGJhNmYtOWNjYi00Njc5LWE2YjEtMWMxZTVmODUwMmMxXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",
+    description:
+      "Two lighthouse keepers try to maintain their sanity while living on a remote and mysterious New England island in the 1890s",
+    rating: 74,
+  },
+  {
+    title: "Eyes Wide Shut",
+    release_date: "1999-07-16",
+    cover_url: "https://m.media-amazon.com/images/M/MV5BMzQ3NWI0NDAtYTE2Zi00YWM4LWIyNGQtZmJlZmZiYzA2MmU2XkEyXkFqcGdeQXVyMjU0MjU2MjI@._V1_.jpg",
+    description:
+      "A Manhattan doctor embarks on a bizarre, night-long odyssey after his wife's admission of unfulfilled longing",
+    rating: 75,
+  },
+  {
+    title: "No Country for Old Men",
+    release_date: "2007-11-09",
+    cover_url: "https://m.media-amazon.com/images/M/MV5BMjA5Njk3MjM4OV5BMl5BanBnXkFtZTcwMTc5MTE1MQ@@._V1_.jpg",
+    description:
+      "Violence and mayhem ensue after a hunter stumbles upon a drug deal gone wrong and more than two million dollars in cash near the Rio Grande",
+    rating: 82,
+  },
+  {
+    title: "The Shining",
+    release_date: "1980-06-23",
+    cover_url: "https://m.media-amazon.com/images/M/MV5BZWFlYmY2MGEtZjVkYS00YzU4LTg0YjQtYzY1ZGE3NTA5NGQxXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg",
+    description:
+      "A family heads to an isolated hotel for the winter where a sinister presence influences the father into violence, while his psychic son sees horrific forebodings from both past and future",
+    rating: 84,
+  },
+  
+];
