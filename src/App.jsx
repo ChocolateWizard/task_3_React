281d0599375715fc0741791d7227170b6ea87724
ChocolateWizard <despotjev@gmail.com>
2023-05-04 15:50:41 +0200

    Experimented with carousel implementation

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import "./App.css";
 import Navbar from "./components/navbar";
 import Movies from "./components/movies";
 import { movies } from "./testing/dummyData";
+import MoviesTest from "./testing/moviesTest";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <main>
-        <Movies movies={movies} />
+        <MoviesTest movies={movies} />
       </main>
     </div>
   );
