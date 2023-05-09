71c2f6349469170e1d9ad3df71efd60255baa55e
ChocolateWizard <despotjev@gmail.com>
2023-05-09 17:08:00 +0200

    Added keys to each movie and show component on homepage

--- src/components/movie/collection/MovieCollection.jsx
+++ src/components/movie/collection/MovieCollection.jsx
@@ -6,7 +6,7 @@ export default function MovieCollection({movies}) {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
         {movies.map((movie) => (
-          <MovieCard movie={movie} />       
+          <MovieCard movie={movie} key={movie.id} />       
         ))}
       </div>
   );
--- src/components/show/collection/ShowCollection.jsx
+++ src/components/show/collection/ShowCollection.jsx
@@ -6,7 +6,7 @@ export default function ShowCollection({shows}) {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
         {shows.map((show) => (
-          <ShowCard show={show} />       
+          <ShowCard show={show} key={show.id} />       
         ))}
       </div>
   );
